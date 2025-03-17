e3a014f779e324fed170dacbb15b3f2cd05945de
Alexsandro Rodrigues <109860864+alexsandev@users.noreply.github.com>
2025-03-17 06:41:49 -0300

    Ajustando comentários do código.

--- excel_file_reader/program.py
+++ excel_file_reader/program.py
@@ -3,13 +3,13 @@ import pandas as pd
 def excel_para_dicionario(diretorio):
     dic = {}
 
-    # Faz a leitura do arquivo Excel e retorna um objeto DataFrame(estrutura de dados bidimensional) 
+    # Faz a leitura do arquivo Excel e retorna um objeto DataFrame(Estrutura de dados bidimensional / Tabela) 
     df = pd.read_excel(diretorio)
 
     # Seleciona as colunas 'id_cliente' e 'quantidade', agrupa por 'id_cliente' e soma as linhas 'quantidade' de cada grupo.
     quantidade_total_cliente = df[['id_cliente','quantidade']].groupby('id_cliente').sum('quantidade')
 
-    # Percorre as linhas da nova tabela
+    # Percorre as linhas da nova Tabela e adiciona cada conjunto de valores 'id_cliente' : 'quantidade' ao dicionario.
     for tupla in quantidade_total_cliente.itertuples():
         id_cliente = tupla[0]
         quantidade = tupla[1]
